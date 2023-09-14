1806b2f1839d661f12a72897ae1639e046dc2bdc
Daniel <danielcrevi@gmail.com>
2023-09-14 18:20:36 +0200

    CAMBIOS SECCION 36

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,17 +1,30 @@
 const { io } = require('../index');
-// MENSAJES DE SOCKETS
+
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Natos y Waor'));
+bands.addBand(new Band('Recycled'));
+bands.addBand(new Band('Metallica'));
+
+console.log(bands);
+
 io.on('connection', client => {
     
     console.log('Cliente contectado');
+
+    client.emit('active-bands',  bands.getBands());
 
     client.on('disconnect', () => { 
         console.log('Cliente desconectado');
     });
 
-    client.on('mensaje', ( payload ) =>  {
+    client.on('emitir-mensaje', ( payload ) =>  {
         console.log('mensaje:' , payload);
-
-        io.emit( 'mensaje', { admin: 'nuevo mensaje'});
+        io.emit('mensaje:', payload)
     });
 
 });
